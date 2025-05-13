05c7bb3ed44ce568bf89cf5e4fdb37dd7185248e
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-13 12:07:23 +0400

    Добавили мемоизацию для даты

--- src/MyComp.jsx
+++ src/MyComp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 // Форматировщик даты с русским языком
 let formatter = new Intl.DateTimeFormat("ru", {
@@ -15,20 +15,38 @@ const getTime = (date) =>
     second: "2-digit",
   });
 
+// Получаем случайное целое число между min и max (включительно)
+const getRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 export const MyComponent = () => {
   const [currentTime, setCurrentTime] = useState(new Date()); // Текущее время
+  const [randomNum, setRandomNum] = useState(getRandomNum(1, 10)); // Случайное число
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    const timeIntervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
+
+    const numIntervalId = setInterval(() => {
+      setRandomNum(getRandomNum(1, 10));
+    }, 2000);
+
     // Очищаем интервал при размонтировании компонента
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(timeIntervalId);
+      clearInterval(numIntervalId);
+    };
   }, []);
 
+  // Получаем текущую дату с заглавной буквы
+  // Мемоизация обработки даты
   const formattedDate = formatter.format(currentTime);
-  const firstLetterCapitalized =
-    formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  const firstLetterCapitalized = useMemo(
+    () => `${formattedDate.charAt(0).toUpperCase()}${formattedDate.slice(1)}`,
+    [formattedDate]
+  );
 
   return (
     <div>
@@ -36,6 +54,8 @@ export const MyComponent = () => {
       <p>{firstLetterCapitalized}</p>
       {/* Отображаем текущее время */}
       <p>{getTime(currentTime)}</p>
+      {/* Отображаем случайное число */}
+      <p>{randomNum}</p>
     </div>
   );
 };
